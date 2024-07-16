562631f0152be43b7f0be5638486a2405d8eced9
Manas Singh <mk9782647@gmail.com>
2024-07-17 01:45:26 +0530

    commit

---
+++ Components/Header.jsx
@@ -0,0 +1,66 @@
+import { assets } from '@/Assets/assets';
+import axios from 'axios';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import Link from 'next/link'; // Import Link from next/link
+
+const Header = () => {
+  const [email, setEmail] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    try {
+      const response = await axios.post('/api/email', formData);
+      if (response.data.success) {
+        toast.success(response.data.msg);
+        setEmail("");
+      } else {
+        toast.error("Error");
+      }
+    } catch (error) {
+      toast.error("Error submitting email");
+    }
+  };
+
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+      <div className='flex justify-between items-center'>
+        <Image src={assets.logo} width={170} alt='' className='w-[120px] sm:w-auto' />
+        <div className='flex gap-4'>
+          <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_black]'>
+            Get Started
+            <Image src={assets.arrow} />
+          </button>
+          <Link href="/admin/addProduct">
+            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_black]'>
+              Admin
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className='text-center my-8'>
+        <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
+        <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, impedit quas consectetur quod fuga iure autem sint nulla ut eaque voluptate tenetur numquam quo facere alias recusandae perferendis harum minima!
+        </p>
+        <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[540px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_black]'>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder='Enter your email'
+            className='pl-4 outline-none'
+          />
+          <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-500 active:text-white'>
+            Subscribe
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
